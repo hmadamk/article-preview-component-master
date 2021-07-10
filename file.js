2be9f2990e87c11dc50a3c20ad7064614da27277
Ahmed Kamel <hmada.kamel2006@gmail.com>
2021-07-10 21:12:14 +0300

    bla bla

---
+++ file.js
@@ -0,0 +1,10 @@
+
+
+
+
+let share = document.querySelector('.share > img');
+
+share.onclick = function(){
+	document.querySelector('.pop').classList.toggle('hide')
+	document.querySelector('.share').classList.toggle('show')
+}
